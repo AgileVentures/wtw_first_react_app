8d207c6df3734c476a99b8da2119606eec96f31e
Thomas <thomas@craftacademy.se>
2021-08-13 06:30:10 +0200

    demo on setting upp and accessing application state using redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import ClassicApp from './ClassicApp'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import ClassicApp from "./ClassicApp";
+import ConnectedComponent from "./ConnectedComponent";
+import HookedComponent from "./HookedComponent";
+import { Provider } from "react-redux";
+import configureStore from "./state/store/configureStore";
+
+const store = configureStore();
+window.store = store;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ClassicApp />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <ConnectedComponent />
+    <HookedComponent />
+  </Provider>,
+  document.getElementById("root")
 );
-
